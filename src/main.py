061cb01c44aabd8973060569f972d365b339f58c
shubhamf0073 <shubhamfufal.8@outlook.com>
2026-01-16 01:12:16 +0530

    Step 1 done

--- src/main.py
+++ src/main.py
@@ -3,16 +3,30 @@ import time
 from video_stream import VideoStream
 
 stream = VideoStream(0)
+prev_time = time.time()
 
 while True:
 	ret, frame = stream.read()
 	if not ret:
-		print("Frame read failed")
 		break
 
-	cv2.imshow("Live feed:", frame)
+	curr_time = time.time()
+	fps = 1 / (curr_time - prev_time)
+	prev_time = curr_time
 
-	if cv2.waitKey(1) & 0xFF == 27: 
+	cv2.putText(
+		frame,
+		f"FPS: {int(fps)}",
+		(20, 40),
+		cv2.FONT_HERSHEY_SIMPLEX,
+		1,
+		(0, 255, 0),
+		2
+	)
+
+	cv2.imshow("Live Feed", frame)
+
+	if cv2.waitKey(1) & 0xFF == 27:
 		break
 
 stream.release()
