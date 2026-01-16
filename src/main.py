6ab1227b9872bfcb742a15e452ddc107895be4f9
shubhamf0073 <shubhamfufal.8@outlook.com>
2026-01-16 14:25:00 +0530

    Boxes added

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import cv2
 import time
 from video_stream import VideoStream
 from fake_detector import get_fake_detections
+from visualizer import Visualizer
 
 stream = VideoStream(0)
-prev_time = time.time()
+# prev_time = time.time()
+visualizer = Visualizer()
 
 while True:
 	ret, frame = stream.read()
@@ -12,22 +14,24 @@ while True:
 		break
 
 	detections = get_fake_detections(frame.shape)
+	requires_cleaning = True
+	frame = visualizer.draw_banner(frame, requires_cleaning)
 
-	for det in detections:
-		x1, y1, x2, y2 = det["bbox"]
-		label = f'{det["class"]} | {det["confidence"]:.2f}'
+	# for det in detections:
+	# 	x1, y1, x2, y2 = det["bbox"]
+	# 	label = f'{det["class"]} | {det["confidence"]:.2f}'
 
-		cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
-		color = (0, 255, 255) if det["class"] == "litter_single" else (0, 0, 255)
-		cv2.putText(
-			frame,
-			label,
-			(x1, y1 - 10),
-			cv2.FONT_HERSHEY_SIMPLEX,
-			0.6,
-			(0, 255, 0),
-			2
-		)
+	# 	cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
+	# 	color = (0, 255, 255) if det["class"] == "litter_single" else (0, 0, 255)
+	# 	cv2.putText(
+	# 		frame,
+	# 		label,
+	# 		(x1, y1 - 10),
+	# 		cv2.FONT_HERSHEY_SIMPLEX,
+	# 		0.6,
+	# 		(0, 255, 0),
+	# 		2
+	# 	)
 
 	cv2.imshow("Live Feed", frame)
 	if cv2.waitKey(1) & 0xFF == 27:
---
+++ src/visualizer.py
@@ -0,0 +1,54 @@
+import cv2 
+
+class Visualizer:
+	def __init__(self):
+		self.color = {
+			"litter_single": (0, 255, 255),
+			"litter_cluster": (0, 0, 255)
+		}
+
+	def draw_detections(self, frame, detections):
+		for det in detections:
+			x1, y1, x2, y2 = det["bbox"]
+			cls = det["class"]
+			conf = det["confidence"]
+
+			color = self.color.get(cls, (255, 255, 255))
+			label = f"{cls} | {conf:.2f}"
+
+			cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+			cv2.putText(
+					frame,
+					label, 
+					(x1, max(y1 - 10, 20)),
+					cv2.FONT_HERSHEY_SIMPLEX,
+					0.6,
+					color,
+					2
+				)
+
+		return frame
+
+	def draw_banner(self, frame, requires_cleaning):
+		h, w, _ = frame.shape
+
+		if requires_cleaning:
+			text = "Area requires cleaning"
+			color = (0, 0, 255)
+
+		else:
+			text = "Road segment clean"
+			color = (0, 255, 0)
+
+		cv2.rectangle(frame, (0, 0), (w, 60), (0, 0, 0), -1)
+		cv2.putText(
+				frame, 
+				text,
+				(int(w * 0.05), 40),
+				cv2.FONT_HERSHEY_SIMPLEX,
+				1.2,
+				color,
+				3
+		)
+
+		return frame
